dba933878d29f3f42289a7db31d3ece56cf4a4d5
AldoHerrera <alherrera@usma.com.pa>
2025-01-14 17:21:16 -0500

    Add Render Blueprint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express, { json } from "express"
 import cors from "cors"
 import recipeRouter from './routes/recipes.js'
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const app = express()
 
 app.use(json())
